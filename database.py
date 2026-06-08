22eac2132f13a66cca5ece912d6419cfd1cb0b9f
dinarosa <166856121+dinarosa@users.noreply.github.com>
2026-06-08 16:05:07 +0700

    Update konfigurasi ke PostgreSQL Cloud

--- database.py
+++ database.py
@@ -2,17 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Sesuai Blueprint: Menggunakan SQLite untuk lingkungan pengembangan lokal
-SQLALCHEMY_DATABASE_URL = "sqlite:///./portofolio_lokal.db"
+# 1. Mengubah alamat brankas dari SQLite lokal ke PostgreSQL Cloud
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_i8jabCDth4rN@ep-autumn-smoke-aos048rl-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
-# connect_args={"check_same_thread": False} wajib ditambahkan agar SQLite tidak memblokir antrean thread di FastAPI
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# 2. Membuat mesin koneksi (connect_args untuk SQLite dihapus karena tidak didukung oleh Postgres)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+# Fungsi jembatan session database
 def get_db():
     db = SessionLocal()
     try:
